accb0f8beeed0d92c20ed2b90de45bd825a0b284
tinit <tinit@Valentina>
2025-11-05 15:34:11 -0500

    commit 1
    En este commit se crearon los paquetes controlado, modelo y vista. En el modelos vista se crearon la interffaz principal, la interfaz cajero y la interfaz administración. La interfaz principal ya es funcional y abre la interfaz cajero o la interfaz administración. En los proximos commits se mostrará como se desarrollan estas 2 interfaces funcionales. 
    Todavía no se ha conectado el programa con la base de datos.

---
+++ src/proyectofinalprogramacion3/ProyectoFinalProgramacion3.java
@@ -0,0 +1,15 @@
+package proyectofinalprogramacion3;
+import vista.interfazPrincipal;
+
+/**
+ *
+ * @author tinit
+ */
+public class ProyectoFinalProgramacion3 {
+    
+    public static void main(String[] args) {
+        interfazPrincipal inter=new interfazPrincipal();
+        inter.setVisible(true);
+    }
+    
+}
